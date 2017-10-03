067de3e7486424fc945f7fd1cf07e81c5ee323a7
Uchiha-Satori <stephen.kit.ho@gmail.com>
2017-10-03 13:35:06 -0400

    Models update
    Record and user updated relationships.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20171003154324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id", using: :btree
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.integer  "chest"
+    t.integer  "tricep"
+    t.integer  "bicep"
+    t.integer  "deadlift"
+    t.integer  "squat"
+    t.text     "personal_notes"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
